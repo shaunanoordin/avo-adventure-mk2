554a3c502688a0e0417e79143c7292803cd764a7
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2019-09-30 03:26:22 +0100

    WIP collision calculations

--- src/avo/map/map.js
+++ src/avo/map/map.js
@@ -72,41 +72,87 @@ class Map {
   checkCollision (element) {
     if (!element || !element.solid) return;
     
-    const tileLT = this.getTile(Math.floor(element.left / this.tileSize),
-                                Math.floor(element.top / this.tileSize));
-    const tileRT = this.getTile(Math.floor(element.right / this.tileSize),
-                                Math.floor(element.top / this.tileSize));
-    const tileLB = this.getTile(Math.floor(element.left / this.tileSize),
-                                Math.floor(element.bottom / this.tileSize));
-    const tileRB = this.getTile(Math.floor(element.right / this.tileSize),
-                                Math.floor(element.bottom / this.tileSize));
+    const size = this.tileSize;
+    
+    const leftCol = Math.floor(element.left / size);
+    const rightCol = Math.floor(element.right / size);
+    const topRow = Math.floor(element.top / size)
+    const bottomRow = Math.floor(element.bottom / size)
+    
+    const tileLT = this.getTile(leftCol, topRow);
+    const tileRT = this.getTile(rightCol, topRow);
+    const tileLB = this.getTile(leftCol, bottomRow);
+    const tileRB = this.getTile(rightCol, bottomRow);
+    
+    let correctionDirectionX = 0;
+    let correctionDirectionY = 0;
+    
+    // Determine which tiles are blocking the element, and in which direction
+    // the correction needs to be done.
+    if (tileLT.wall) { correctionDirectionX++; correctionDirectionY++; }
+    if (tileRT.wall) { correctionDirectionX--; correctionDirectionY++; }
+    if (tileLB.wall) { correctionDirectionX++; correctionDirectionY--; }
+    if (tileRB.wall) { correctionDirectionX--; correctionDirectionY--; }
     
     let correctionX = 0;
     let correctionY = 0;
     
-    if (tileLT.wall) { correctionX++; correctionY++; }
-    if (tileRT.wall) { correctionX--; correctionY++; }
-    if (tileLB.wall) { correctionX++; correctionY--; }
-    if (tileRB.wall) { correctionX--; correctionY--; }
+    // Determine how far the correction needs to be made.
+    // (i.e. determine how 'deep' the element pushed into the blocking tile.)
+    if (correctionDirectionX > 0) {
+      const tileEdgeX = leftCol * size + size;
+      correctionX = tileEdgeX - element.left;
+    } else if (correctionDirectionX < 0) {
+      const tileEdgeX = rightCol * size;
+      correctionX = tileEdgeX - element.right;
+    }
     
-
+    if (correctionDirectionY > 0) {
+      const tileEdgeY = topRow * size + size;
+      correctionY = tileEdgeY - element.top;
+    } else if (correctionDirectionY < 0) {
+      const tileEdgeY = bottomRow * size;
+      correctionY = tileEdgeY - element.bottom;
+    }
+    
+    // Only correct on one axis at a time.
+    const correctionXmagnitude = Math.abs(correctionX);
+    const correctionYmagnitude = Math.abs(correctionY);
+    
+    // DEBUG
+    if (element === this._app.playerActor)
+      console.log('x: ', correctionX, ' || y: ', correctionY);
+    
+    if (correctionXmagnitude > 0 && correctionYmagnitude > 0) {
+      if (correctionXmagnitude > correctionYmagnitude) correctionX = 0;
+      else if (correctionXmagnitude < correctionYmagnitude) correctionY = 0;
+    }
+    
+    let collisionCorrectedX = element.x + correctionX;
+    let collisionCorrectedY = element.y + correctionY;
+    
     // DEBUG
     // if (element === this._app.playerActor) console.log(tileLT.wall);
     
     return {
-      x: element.x + correctionX,
-      y: element.y + correctionY,
+      x: collisionCorrectedX,
+      y: collisionCorrectedY,
     }
   }
   
   getTile (col, row) {
     const index = row * this.width + col;
+    const size = this.tileSize;
     const tileValue = this.tiles[index] || '';
     const tileType = this.tileTypes[tileValue] || DEFAULT_TILE_TYPE;
     
     return {
       col,
       row,
+      // left: col * size,
+      // right: col * size + size,
+      // top: col * size,
+      // bottom: col * size + size,
       ...tileType,
     };
   }
